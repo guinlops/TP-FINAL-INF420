38df34258b21fa8d495e61f3b5942741ca0405eb
Guilherme Nunes Lopes <gui.nunes253@gmail.com>
2024-08-25 18:13:36 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # TP-FINAL-INF420
 Repositório usado para o trabalho Final de INF420
+
+.ENV: Necessário criar um arquivo separado .env e colocar: 
+    *obs, client_id e Client_secret sao encontrados no site da api do spotify
+    CLIENT_ID=""
+    CLIENT_SECRET=""
---
+++ get_tracks.py
@@ -0,0 +1,121 @@
+import json
+from dotenv import load_dotenv
+import os
+import base64
+from requests import post,get
+
+load_dotenv()
+client_id=os.getenv("CLIENT_ID")
+client_secret=os.getenv("CLIENT_SECRET")
+
+
+def get_token():
+    auth_string=client_id+":"+ client_secret
+    auth_bytes=auth_string.encode("utf-8")
+    auth_base64=str(base64.b64encode(auth_bytes),"utf-8")
+    url="https://accounts.spotify.com/api/token"
+    headers={
+        "Authorization": "Basic "+ auth_base64,
+        "Content-Type":"application/x-www-form-urlencoded"
+    }
+    data={"grant_type":"client_credentials"}
+    result=post(url,headers=headers,data=data)
+    json_result=json.loads(result.content)
+    token=json_result["access_token"]
+    return token
+token=get_token()
+
+def get_auth_header(token):
+    return{"Authorization":"Bearer "+ token}
+
+def search_for_artist(token, artist_name):
+    url="https://api.spotify.com/v1/search"
+    headers=get_auth_header(token)
+    query=f"?q={artist_name}&type=artist&limit=1"
+
+    query_url=url+query
+    result=get(query_url,headers=headers)
+    json_result=json.loads(result.content)["artists"]["items"]
+    #print(json_result)
+    if(len(json_result)==0):
+        print("No artist with this name exists...")
+        return None
+    
+    return json_result[0]
+
+def get_songs_by_artist(token,artist_id):
+    url=f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks?country=US"
+    headers=get_auth_header(token)
+    result=get(url,headers=headers)
+    json_result=json.loads(result.content)["tracks"]
+    return json_result
+
+
+def get_playlist_tracks(token, playlist_id):
+    url = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks"
+    headers = get_auth_header(token)
+
+    result = get(url, headers=headers)
+    json_result = json.loads(result.content) 
+
+    tracks = json_result["items"]
+    return tracks
+
+
+def get_playlist_obj(token, playlist_name):
+  """
+  Busca por uma playlist pública no Spotify.
+
+  Args:
+    token: Token de acesso da sua aplicação Spotify.
+    query: Termo de busca para a playlist.
+
+  Returns:
+    Um dicionário com informações sobre a playlist encontrada, ou None se não encontrar.
+  """
+
+  url = "https://api.spotify.com/v1/search"
+  headers = get_auth_header(token)
+  params = {"q": playlist_name, "type": "playlist"}
+
+  response = get(url, headers=headers, params=params)
+  response.raise_for_status()
+
+  json_response = response.json()
+  playlists = json_response.get('playlists', {}).get('items', [])
+
+  if playlists:
+    return playlists[0]  # Retorna a primeira playlist encontrada
+  else:
+    return None
+
+
+
+def print_tracks(tracks):
+    
+    try:
+        with open("arquivo.txt", "w") as arquivo:
+            print("Track_name; Artist_Name",file=arquivo)
+            for track in tracks:
+                song_name = track["track"]["name"]
+                artist_name = track["track"]["artists"][0]["name"]
+                print(f"{song_name};{artist_name}", file=arquivo)
+    except FileNotFoundError:
+        print("Erro: O arquivo não foi encontrado.")
+    except PermissionError:
+        print("Erro: Permissão negada para acessar o arquivo.")
+    except Exception as e:
+        print(f"Erro inesperado ao escrever no arquivo: {str(e)}")
+
+def get_playlist(playlist_name):
+     playlist_obj=get_playlist_obj(token,playlist_name)
+     playlist_id=playlist_obj['id']
+     tracks=get_playlist_tracks(token,playlist_id)
+     print_tracks(tracks)
+
+
+def main():
+    get_playlist("lofi beats") 
+
+if __name__ == '__main__':
+    main()
